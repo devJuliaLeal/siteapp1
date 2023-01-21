547c6b44eeb25e8d7d6fffa0bbf75459c9b7f10c
devJuliaLeal <juliaassuncaoleal@gmail.com>
2023-01-20 21:54:08 -0300

    commit

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+  const name ="Julia"
   return (
     <div className="App">
       <h1> Olá</h1>
       <p>meu primeiro app</p>
+      <p>Olá, {name}</p>
     </div>
   );
 }
