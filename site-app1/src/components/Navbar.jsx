aed9f44b5b58e64881d76c852362a6f93cdaa833
devJuliaLeal <juliaassuncaoleal@gmail.com>
2023-01-25 09:09:58 -0300

    commit

---
+++ site-app1/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div8 className='text-white'>
+        <h1>className=''> React.</h1>
+    </div>
+  )
+}
+
+export default Navbar
