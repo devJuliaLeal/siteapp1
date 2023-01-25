6c4f3c02073eeb47ed3c354760978e513dcfef3c
devJuliaLeal <juliaassuncaoleal@gmail.com>
2023-01-25 11:16:13 -0300

    commit

--- site-app-1/src/App.jsx
+++ site-app-1/src/App.jsx
@@ -1,12 +1,31 @@
 import React from 'react'
+import Header from './components/header/Header'
+import About from './components/about/About'
+import Contact from './components/contact/Contact'
+import Experience from './components/experience/experience'
+import Footer from './components/footer/Footer'
+import Nav from './components/nav/Nav'
+import Portifolio from './components/portfolio/portfolio'
+import Testimonials from './components/testimonials/Testimonials'
 
 
-const Apps = () => {
+
+
+
+const App = () => {
   return (
- <>
- 
- </>
+    <div>
+      <Header/>
+      <About/>
+      <Contact/>
+      <Experience/>
+      <Footer/>
+      <Nav/>
+      <Portifolio/>
+      <Testimonials/>
+
+    </div>
   )
 }
 
-export default Apps
+export default App
--- site-app-1/src/index.js
+++ site-app-1/src/index.js
@@ -1,5 +1,5 @@
 import ReactDom from 'react-dom';
-import App from './App';
+import App from './App.jsx';
 import './index.css';
-import Apps from './App.jsx';
+
 ReactDom.render(<App/>, document.querySelector("#root"));
