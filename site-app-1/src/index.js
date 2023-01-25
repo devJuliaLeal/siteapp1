421edfd5de75ec40fb8cd047fcb41ea339996bcf
devJuliaLeal <juliaassuncaoleal@gmail.com>
2023-01-25 10:33:40 -0300

    commit

--- site-app-1/src/index.js
+++ site-app-1/src/index.js
@@ -1,12 +1,4 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDom from 'react-dom';
 import App from './App';
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import Apps from './App.jsx';
+ReactDom.render(<App/>, document.querySelector("#root"));
