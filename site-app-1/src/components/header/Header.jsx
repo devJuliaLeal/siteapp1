81e8a81a563e89c7ce45551ea98aee4e08a81818
devJuliaLeal <juliaassuncaoleal@gmail.com>
2023-01-27 15:46:50 -0300

    commit

--- site-app-1/src/components/header/Header.jsx
+++ site-app-1/src/components/header/Header.jsx
@@ -16,9 +16,9 @@ const Header = () => {
      <HeaderSocials/>
      
 
-     
+     <div className='imagem'>
       <img src={IM} alt='image'></img>
-    
+     </div>
      <a href="#contact" className='scroll_down'> Scroll Down</a>
      </div>
     </header>
