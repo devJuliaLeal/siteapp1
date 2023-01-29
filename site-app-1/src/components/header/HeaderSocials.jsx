23bb7a370c1fb1015c969b40dde4a50e3803bad9
devJuliaLeal <juliaassuncaoleal@gmail.com>
2023-01-28 21:06:36 -0300

    commit

--- site-app-1/src/components/header/HeaderSocials.jsx
+++ site-app-1/src/components/header/HeaderSocials.jsx
@@ -5,9 +5,11 @@ import {AiOutlineInstagram} from 'react-icons/ai'
 import {AiOutlineWhatsApp} from 'react-icons/ai'
 
 
+
+
 const HeaderSocials = () => {
   return (
-    <div className='header__socials'>
+      <div className='header__socials' >
         <a  href='https://linkedin.com'target='_blank' ><CiLinkedin/></a>
         <a href='https://github.com'target='_blank'><FaGithubAlt/></a>
         <a href='https://instagram.com'target='_blank'><AiOutlineInstagram/></a>
