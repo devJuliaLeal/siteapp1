31f97d4f8da853d40c4331b7bea42aea16952ee3
devJuliaLeal <juliaassuncaoleal@gmail.com>
2023-01-29 14:01:48 -0300

    commit

--- site-app-1/src/components/nav/Nav.jsx
+++ site-app-1/src/components/nav/Nav.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import './nav.css'
+import {RiHomeLine} from 'react-icons/ri'
+import {AiOutlineUser} from 'react-icons/ai'
 
 const nav = () => {
   return (
-    <div>nav</div>
+    <div>
+      <a href='#'><RiHomeLine/></a>
+      <a href='#about'><AiOutlineUser/></a>
+      <a href='#experience'><RiHomeLine/></a>
+      <a href='#services'><RiHomeLine/></a>
+      <a href='#contact'><RiHomeLine/></a>
+  
+
+
+
+
+    </div>
   )
 }
 
