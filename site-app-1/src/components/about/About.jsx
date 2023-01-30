ac28451d6f0cec5c5756bb3f0257ce6b31b057d5
devJuliaLeal <juliaassuncaoleal@gmail.com>
2023-01-30 11:11:39 -0300

    commit

--- site-app-1/src/components/about/About.jsx
+++ site-app-1/src/components/about/About.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import './about.css'
+import img from '../../assets/meninadancando.gif'
 
 const About = () => {
   return (
-    <section id='about'>About</section>
+    <section id='about'>
+      <h5>Get To know</h5>
+      <h2>About me</h2>
+      <div className='container about__container'>
+        <div className='about__me'>
+          <img src={img}></img>
+        </div>
+        <div className='about__content'>
+
+        </div>
+      </div>
+      </section>
   )
 }
 
