be819a6b068808344cd3f4ac62b6ccab1a201d39
devJuliaLeal <juliaassuncaoleal@gmail.com>
2023-01-30 15:28:23 -0300

    commit

--- site-app-1/src/components/about/About.jsx
+++ site-app-1/src/components/about/About.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './about.css'
-import img from '../../assets/meninadancando.gif'
+import img from '../../assets/mulher.png'
 import {FaAward} from 'react-icons/fa'
 import {FiUsers} from 'react-icons/fi'
 import {AiOutlineFundProjectionScreen} from 'react-icons/ai'
