f18bd8cc097bfd1b1a9c6fb148cbe5af1d1ccf68
devJuliaLeal <juliaassuncaoleal@gmail.com>
2023-01-31 11:08:05 -0300

    commit

--- site-app-1/src/components/experience/experience.jsx
+++ site-app-1/src/components/experience/experience.jsx
@@ -1,9 +1,44 @@
 import React from 'react'
 import './experience.css'
+import {BsFillPatchCheckFill} from 'react-icons/bs'
 
 const experience = () => {
   return (
-    <section id='experience'> Experience</section>
+    <section id='experience'>
+      <h5>What Skills I Have</h5> 
+      <h2>My experience</h2>
+      <div className='container experience__container'>
+        <div className='experience__frontend'>
+          <h3> Frontend Experience</h3>
+          <div className='experience__content'></div>
+            <article className='experience__details'>
+            <BsFillPatchCheckFill/>
+            <h4> HTML</h4>
+            <h5 className='text-light'>Experienced</h5>
+            <BsFillPatchCheckFill/>
+            <h4> CSS</h4>
+            <h5 className='text-light'>Experienced</h5>
+            <BsFillPatchCheckFill/>
+            <h4> JavaScript</h4>
+            <h5 className='text-light'>Experienced</h5>
+            <BsFillPatchCheckFill/>
+            <h4> React</h4>
+            <h5 className='text-light'>Experienced</h5>
+            <BsFillPatchCheckFill/>
+            <h4> Tailwind</h4>
+            <h5 className='text-light'>Experienced</h5>
+            </article>
+            
+        </div>
+{/*END OF FRONTEND*/}
+
+
+
+        <div className='experience__backend'></div>
+
+        <div className='experience__content'></div>
+      </div>
+      </section>
   )
 }
 
