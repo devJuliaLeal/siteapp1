bb51c2eb52324f205e153eeed0ca0e3453e0bd8c
devJuliaLeal <juliaassuncaoleal@gmail.com>
2023-02-01 09:01:41 -0300

    commit

--- site-app-1/src/components/experience/experience.jsx
+++ site-app-1/src/components/experience/experience.jsx
@@ -6,11 +6,16 @@ const experience = () => {
   return (
     <section id='experience'>
       <h5>What Skills I Have</h5> 
+
+
       <h2>My experience</h2>
+
+ {/* FRONTEND*/}     
+      
       <div className='container experience__container'>
         <div className='experience__frontend'>
           <h3> Frontend Experience</h3>
-          <div className='experience__content'></div>
+          <div className='experience__content'>
             <article className='experience__details'>
             <BsFillPatchCheckFill/>
             <h4> HTML</h4>
@@ -28,17 +33,36 @@ const experience = () => {
             <h4> Tailwind</h4>
             <h5 className='text-light'>Experienced</h5>
             </article>
-            
+            </div>
         </div>
-{/*END OF FRONTEND*/}
+{/* BACKEND*/}
 
-
-
-        <div className='experience__backend'></div>
-
-        <div className='experience__content'></div>
-      </div>
-      </section>
+        
+        <div className='experience__backend'>
+        <h3> Frontend Experience</h3>
+          <div className='experience__content'>
+            <article className='experience__details'>
+            <BsFillPatchCheckFill/>
+            <h4> HTML</h4>
+            <h5 className='text-light'>Experienced</h5>
+            <BsFillPatchCheckFill/>
+            <h4> CSS</h4>
+            <h5 className='text-light'>Experienced</h5>
+            <BsFillPatchCheckFill/>
+            <h4> JavaScript</h4>
+            <h5 className='text-light'>Experienced</h5>
+            <BsFillPatchCheckFill/>
+            <h4> React</h4>
+            <h5 className='text-light'>Experienced</h5>
+            <BsFillPatchCheckFill/>
+            <h4> Tailwind</h4>
+            <h5 className='text-light'>Experienced</h5>
+            </article>
+          </div>
+        </div>
+       </div>
+    
+ </section>
   )
 }
 
