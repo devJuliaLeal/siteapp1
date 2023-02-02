4cd0846a78d61d8175c086570a939689bef6519c
devJuliaLeal <juliaassuncaoleal@gmail.com>
2023-02-02 10:29:10 -0300

    commit

--- site-app-1/src/components/services/Services.jsx
+++ site-app-1/src/components/services/Services.jsx
@@ -7,67 +7,38 @@ const Services = () => {
     <section id='services'>
       <h5>What I Offer</h5>
       <h2>Services</h2>
-      <div className='container services__container'>
+     
+{/*UI/UX*/}
+
+     
+      <div className='container'>
         <article className='service'>
           <div className='service_head'>
             <h3>UI/UX Design</h3>
           </div>
           <ul className='serivice__list'>
             <li><BsCheckCircle className='service__list-icon'/>
-            <p> Diz-me com quem andas e te direi se vou contigo.</p>
+              <p> Diz-me com quem andas e te direi se vou contigo.</p>
             </li>
             <li><BsCheckCircle className='service__list-icon'/>
-            <p> Diz-me com quem andas e te direi se vou contigo.</p>
+               <p> Diz-me com quem andas e te direi se vou contigo.</p>
             </li>
             <li><BsCheckCircle className='service__list-icon'/>
-            <p> Diz-me com quem andas e te direi se vou contigo.. </p>
+                <p> Diz-me com quem andas e te direi se vou contigo.. </p>
             </li>
-            <li><BsCheckCircle className='service__list-icon'/>
-            <p> Diz-me com quem andas e te direi se vou contigo.</p>
-            </li>
-            <li><BsCheckCircle className='service__list-icon'/>
-            <p> Diz-me com quem andas e te direi se vou contigo.</p>
-            </li>
-
+              <li><BsCheckCircle className='service__list-icon'/>
+                <p> Diz-me com quem andas e te direi se vou contigo.</p>
+             </li>
+              <li><BsCheckCircle className='service__list-icon'/>
+                <p> Diz-me com quem andas e te direi se vou contigo.</p>
+              </li>
           </ul>
-
-          
-
-
         </article>
-        </div>
+    
 
-      <div className='container services__container'>
-        <article className='service'>
-          <div className='service_head'>
-            <h3>UI/UX Design</h3>
-          </div>
-          <ul className='serivice__list'>
-            <li><BsCheckCircle className='service__list-icon'/>
-            <p> Diz-me com quem andas e te direi se vou contigo.</p>
-            </li>
-            <li><BsCheckCircle className='service__list-icon'/>
-            <p> Diz-me com quem andas e te direi se vou contigo.</p>
-            </li>
-            <li><BsCheckCircle className='service__list-icon'/>
-            <p> Diz-me com quem andas e te direi se vou contigo.. </p>
-            </li>
-            <li><BsCheckCircle className='service__list-icon'/>
-            <p> Diz-me com quem andas e te direi se vou contigo.</p>
-            </li>
-            <li><BsCheckCircle className='service__list-icon'/>
-            <p> Diz-me com quem andas e te direi se vou contigo.</p>
-            </li>
-
-          </ul>
-
-          
-
-
-        </article>
-        </div>
+{/*WEB DEVELOPMENT*/}
         
-        <div className='container services__container'>
+        
         <article className='service'>
           <div className='service_head'>
             <h3>Web Development</h3>
@@ -90,47 +61,41 @@ const Services = () => {
             </li>
 
           </ul>
+         </article>
+           
 
-          
 
 
-        </article>
-        </div>
-       < div className='container services__container'>
+{/*CONTENT CREATION */}
+      
         <article className='service'>
           <div className='service_head'>
             <h3>Content Creation</h3>
           </div>
           <ul className='serivice__list'>
             <li><BsCheckCircle className='service__list-icon'/>
-            <p> Diz-me com quem andas e te direi se vou contigo.</p>
+              <p> Diz-me com quem andas e te direi se vou contigo.</p>
             </li>
             <li><BsCheckCircle className='service__list-icon'/>
-            <p> Diz-me com quem andas e te direi se vou contigo.</p>
+              <p> Diz-me com quem andas e te direi se vou contigo.</p>
             </li>
             <li><BsCheckCircle className='service__list-icon'/>
-            <p> Diz-me com quem andas e te direi se vou contigo.. </p>
+              <p> Diz-me com quem andas e te direi se vou contigo.. </p>
             </li>
             <li><BsCheckCircle className='service__list-icon'/>
-            <p> Diz-me com quem andas e te direi se vou contigo.</p>
+              <p> Diz-me com quem andas e te direi se vou contigo.</p>
             </li>
             <li><BsCheckCircle className='service__list-icon'/>
-            <p> Diz-me com quem andas e te direi se vou contigo.</p>
+              <p> Diz-me com quem andas e te direi se vou contigo.</p>
             </li>
-
           </ul>
-
-          
-
-
         </article>
         </div>
-
     </section>
 
         
-
-
+ 
+  
 
   )
 }
