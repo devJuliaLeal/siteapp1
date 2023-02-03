ea32744a9667d299bd224aef2ed8f5dbbac461f6
devJuliaLeal <juliaassuncaoleal@gmail.com>
2023-02-03 14:20:45 -0300

    commit

--- site-app-1/src/App.jsx
+++ site-app-1/src/App.jsx
@@ -1,13 +1,9 @@
 import React from 'react'
 import Header from './components/header/Header'
 import About from './components/about/About'
-import Contact from './components/contact/Contact'
 import Experience from './components/experience/experience'
-import Footer from './components/footer/Footer'
 import Nav from './components/nav/Nav'
-import Portifolio from './components/portfolio/portfolio'
-import Testimonials from './components/testimonials/Testimonials'
-import Services from './components/services/Services'
+
 
 
 
@@ -20,11 +16,11 @@ const App = () => {
       <Nav/>
       <About/>
       <Experience/>
-      <Services/>
-      <Portifolio/>
-      <Testimonials/>
-      <Contact/> 
-      <Footer/>
+      
+   
+    
+     
+      
       
      
     
--- site-app-1/src/components/nav/Nav.jsx
+++ site-app-1/src/components/nav/Nav.jsx
@@ -3,8 +3,8 @@ import './nav.css'
 import {RiHomeLine} from 'react-icons/ri'
 import {AiOutlineUser} from 'react-icons/ai'
 import {AiOutlineBook} from 'react-icons/ai'
-import {RiServiceLine} from 'react-icons/ri'
-import {BiMessageSquareDots} from 'react-icons/bi'
+
+
 
 const nav = () => {
   return (
@@ -13,8 +13,8 @@ const nav = () => {
       <a href='#' ><RiHomeLine/></a>
       <a href='#about'><AiOutlineUser/></a>
       <a href='#experience'><AiOutlineBook/></a>
-      <a href='#services'><RiServiceLine/></a>
-      <a href='#contact'><BiMessageSquareDots/></a>
+  
+     
   
 
 
